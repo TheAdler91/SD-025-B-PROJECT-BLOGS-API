70661065826dde49e087fde1240b54f95d63650e
Your NamePhilip <philip.adler@hotmail.com>
2023-03-03 18:01:06 -0300

    Feat: Model User added

---
+++ src/models/User.js
@@ -0,0 +1,26 @@
+module.exports = (sequelize, DataTypes) => {
+    const User = sequelize.define('User', {
+      id: {
+        type: DataTypes.INTEGER,
+        primaryKey: true,
+        autoIncrement: true,
+      },
+      displayName: DataTypes.STRING,
+      email: DataTypes.STRING,
+      password: DataTypes.STRING,
+      image: DataTypes.STRING,
+    },
+      {
+        timestamps: false,
+        underscored: true,
+        tableName: 'users',
+      });
+  
+    User.associate = (models) => {
+      User.hasMany(models.BlogPost, {
+        foreignKey: 'userId', as: 'blogPosts'
+      });
+    }
+  
+    return User;
+  }

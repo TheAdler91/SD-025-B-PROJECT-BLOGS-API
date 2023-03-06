830c16f97097a2a8b4569cdf49c5127c562413fb
Your NamePhilip <philip.adler@hotmail.com>
2023-03-06 20:10:43 -0300

    Feat: middleware for token validation ready

--- src/utils/token.js
+++ src/utils/token.js
@@ -9,6 +9,27 @@ const genarateToken = (data) => {
   return token;
 };
 
+const tokenValidation = (req, res, next) => {
+  const token = req.headers.authorization;
+  if (!token) {
+    return res.status(401).json({
+      message: 'Token not found',
+    });
+  }
+  try {
+    const { payload } = jwt.verify(token, secret);
+
+    req.user = payload;
+
+     next();
+  } catch (err) {
+    return res.status(401).json({
+      message: 'Expired or invalid token',
+    });
+  }
+};
+
 module.exports = {
     genarateToken,
+    tokenValidation,
 };

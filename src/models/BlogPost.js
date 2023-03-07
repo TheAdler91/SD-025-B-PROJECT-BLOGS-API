5f9df6e7c13a0cf86de44196e126c553acf61fdd
Your NamePhilip <philip.adler@hotmail.com>
2023-03-07 18:10:20 -0300

    Feat: Model BlogPost created and associated with Model User

---
+++ src/models/BlogPost.js
@@ -0,0 +1,33 @@
+module.exports = (sequelize, DataTypes) => {
+    const BlogPost = sequelize.define('BlogPost', {
+      id: {
+        type: DataTypes.INTEGER,
+        primaryKey: true,
+        autoIncrement: true,
+      },
+      title: DataTypes.STRING,
+      content: DataTypes.STRING,
+      userId: DataTypes.INTEGER,
+      published: {
+        type: DataTypes.DATE,
+        defaultValue: DataTypes.NOW,
+      },
+      updated: {
+        type: DataTypes.DATE,
+        defaultValue: DataTypes.NOW,
+      },
+    },
+      {
+        timestamps: false,
+        underscored: true,
+        tableName: 'blog_posts',
+      });
+  
+    BlogPost.associate = (models) => {
+      BlogPost.belongsTo(models.User, {
+        foreignKey: 'userId', as: 'user'
+      });
+    }
+  
+    return BlogPost;
+  };

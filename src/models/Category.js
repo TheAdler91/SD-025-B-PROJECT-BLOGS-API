98a5a46f01363439c39e709b203a5a1f7b367d85
Your NamePhilip <philip.adler@hotmail.com>
2023-03-07 16:50:01 -0300

    Feat: Model Category created

---
+++ src/models/Category.js
@@ -0,0 +1,17 @@
+module.exports = (sequelize, DataTypes) => {
+    const Category = sequelize.define('Category', {
+      id: {
+        type: DataTypes.INTEGER,
+        primaryKey: true,
+        autoIncrement: true,
+      },
+      name: DataTypes.STRING,
+    },
+      {
+        timestamps: false,
+        underscored: true,
+        tableName: 'categories',
+      });
+
+    return Category;
+  }
